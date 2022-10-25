aeca18f254a9212c29adaf5b936df2b2c65f4694
gleb-bread <glebhets123@gmail.com>
2022-10-26 02:05:47 +0300

    fix scroll-bg

--- js/scroll-bg.js
+++ js/scroll-bg.js
@@ -1,23 +1,38 @@
 document.addEventListener("DOMContentLoaded", function () {
-    let stollDif = -1;
+    let startScroll = 0;
     let linkBackground = document.getElementsByClassName('big__link__container')[0];
     let linkBackground1 = document.getElementsByClassName('big__link__container')[1];
     document.addEventListener("scroll", () => {
         let scroll = window.pageYOffset;
-        if (scroll >= stollDif) {
-            let backgroundPosY = Number(window.getComputedStyle(linkBackground).backgroundPositionY.split(',')[0].split('%')[0]);
-            if (backgroundPosY < 100) {
-                linkBackground.style.backgroundPositionY = backgroundPosY + 0.3 + "%";
-                linkBackground1.style.backgroundPositionY = backgroundPosY + 0.3 + "%";
-            }
+        if (scroll - startScroll > 0) {
+            srcollingBackgroundUp(linkBackground);
+            srcollingBackgroundUp(linkBackground1);
+        } else {
+            srcollingBackgroundDown(linkBackground);
+            srcollingBackgroundDown(linkBackground1);
         }
-        else {
-            let backgroundPosY = Number(window.getComputedStyle(linkBackground).backgroundPositionY.split(',')[0].split('%')[0]);
-            if (backgroundPosY > 0) {
-                linkBackground.style.backgroundPositionY = backgroundPosY - 0.3 + "%";
-                linkBackground1.style.backgroundPositionY = backgroundPosY - 0.3 + "%";
-            }
-        }
-        stollDif = window.pageYOffset;
+        startScroll = window.pageYOffset;
     });
+
+    function returnX(width) {
+        return (width >= 1025) ? 0.4 : (width >= 768) ? 1 : (width >= 320) ? 2 : 0;
+    }
+
+    function srcollingBackgroundUp(elBackground) {
+        let visibleHeight = document.documentElement.clientHeight;
+        if (elBackground.getBoundingClientRect().top <= visibleHeight) {
+            let coof = returnX(document.documentElement.clientWidth);
+            let backgroundPosY = Number(window.getComputedStyle(elBackground).backgroundPositionY.split(',')[0].split('%')[0]);
+            elBackground.style.backgroundPositionY = backgroundPosY + coof + "%";
+            elBackground.style.backgroundPositionY = backgroundPosY + coof + "%";
+        }
+    }
+    function srcollingBackgroundDown(elBackground) {
+        if (elBackground.getBoundingClientRect().bottom + 50 >= 0) {
+            let coof = returnX(document.documentElement.clientWidth);
+            let backgroundPosY = Number(window.getComputedStyle(elBackground).backgroundPositionY.split(',')[0].split('%')[0]);
+            elBackground.style.backgroundPositionY = backgroundPosY - coof + "%";
+            elBackground.style.backgroundPositionY = backgroundPosY - coof + "%";
+        }
+    }
 });
